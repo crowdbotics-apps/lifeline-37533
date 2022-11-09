92c25ee9ba9ca71aa87b6133c2f867e14bd4410e
Crowdbotics <team@crowdbotics.com>
2022-11-09 03:53:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lifeline_37533/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lifeline_37533;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lifeline_37533";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lifeline_37533",
+  url: "https://lifeline_37533.botics.co",
+  api: "https://lifeline_37533.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
